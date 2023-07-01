9e41f8afa4a079941d564ea2a19ab12c6bb8f1a6
Toyly <99834084+ToylyMyratgeldiyev@users.noreply.github.com>
2023-07-01 11:34:57 +0300

    add pic header

---
+++ src/pages/Tickets.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+const maskGroup = require('../img/maskGroup.jpg')
+
+ const Tickets:React.FC = () => {
+  return (
+    <div>
+      
+      <img src={maskGroup} alt="image" />
+
+    </div>
+  )
+}
+
+export default Tickets
